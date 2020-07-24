d954d6a8286da7331c22f1e6aba7f63c50f8fdf3
diego1982chile <diego.friedman@gmail.com>
2020-07-24 14:19:22 -0400

    asdasd

---
+++ jmetal-exec/src/main/java/org/uma/jmetal/runner/singleobjective/GenerationalGeneticAlgorithmStockMarketIntegerRunner.java
@@ -0,0 +1,82 @@
+package org.uma.jmetal.runner.singleobjective;
+
+import org.uma.jmetal.algorithm.Algorithm;
+import org.uma.jmetal.algorithm.singleobjective.geneticalgorithm.GeneticAlgorithmBuilder;
+import org.uma.jmetal.operator.CrossoverOperator;
+import org.uma.jmetal.operator.MutationOperator;
+import org.uma.jmetal.operator.SelectionOperator;
+import org.uma.jmetal.operator.impl.crossover.IntegerSBXCrossover;
+import org.uma.jmetal.operator.impl.crossover.SinglePointCrossover;
+import org.uma.jmetal.operator.impl.mutation.BitFlipMutation;
+import org.uma.jmetal.operator.impl.mutation.IntegerPolynomialMutation;
+import org.uma.jmetal.operator.impl.selection.BinaryTournamentSelection;
+import org.uma.jmetal.problem.BinaryProblem;
+import org.uma.jmetal.problem.IntegerProblem;
+import org.uma.jmetal.problem.singleobjective.trading.StockMarket;
+import org.uma.jmetal.problem.singleobjective.trading.StockMarketInteger;
+import org.uma.jmetal.solution.BinarySolution;
+import org.uma.jmetal.solution.IntegerSolution;
+import org.uma.jmetal.util.AlgorithmRunner;
+import org.uma.jmetal.util.JMetalLogger;
+import org.uma.jmetal.util.comparator.RankingAndCrowdingDistanceComparator;
+import org.uma.jmetal.util.fileoutput.SolutionListOutput;
+import org.uma.jmetal.util.fileoutput.impl.DefaultFileOutputContext;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Class to configure and run a generational genetic algorithm. The target problem is StockMarket.
+ *
+ * @author Antonio J. Nebro <antonio@lcc.uma.es>
+ */
+public class GenerationalGeneticAlgorithmStockMarketIntegerRunner {
+  /**
+   * Usage: java org.uma.jmetal.runner.singleobjective.BinaryGenerationalGeneticAlgorithmRunner
+   */
+  public static void main(String[] args) throws Exception {
+
+    IntegerProblem problem;
+    Algorithm<IntegerSolution> algorithm;
+    CrossoverOperator<IntegerSolution> crossover;
+    MutationOperator<IntegerSolution> mutation;
+    SelectionOperator<List<IntegerSolution>, IntegerSolution> selection;
+
+    //problem = new TSP("/tspInstances/kroA100.tsp");
+    problem = new StockMarketInteger();
+
+    crossover = new IntegerSBXCrossover(0.9, 20.0) ;
+
+    double mutationProbability = 1.0 / problem.getNumberOfVariables() ;
+    mutation = new IntegerPolynomialMutation(mutationProbability, 20.0) ;
+
+    selection = new BinaryTournamentSelection<IntegerSolution>(new RankingAndCrowdingDistanceComparator<IntegerSolution>());
+
+    algorithm = new GeneticAlgorithmBuilder<>(problem, crossover, mutation)
+            .setPopulationSize(5000)
+            //.setMaxEvaluations(250000)
+            .setMaxEvaluations(10000)
+            .setSelectionOperator(selection)
+            .build() ;
+
+    AlgorithmRunner algorithmRunner = new AlgorithmRunner.Executor(algorithm)
+            .execute() ;
+
+    IntegerSolution solution = algorithm.getResult() ;
+    List<IntegerSolution> population = new ArrayList<>(1) ;
+    population.add(solution) ;
+
+    long computingTime = algorithmRunner.getComputingTime() ;
+
+    new SolutionListOutput(population)
+            .setSeparator("\t")
+            .setVarFileOutputContext(new DefaultFileOutputContext("VAR.tsv"))
+            .setFunFileOutputContext(new DefaultFileOutputContext("FUN.tsv"))
+            .print();
+
+    JMetalLogger.logger.info("Total execution time: " + computingTime + "ms");
+    JMetalLogger.logger.info("Objectives values have been written to file FUN.tsv");
+    JMetalLogger.logger.info("Variables values have been written to file VAR.tsv");
+
+  }
+}
---
+++ jmetal-problem/src/main/java/org/uma/jmetal/problem/singleobjective/trading/StockMarketInteger.java
@@ -0,0 +1,94 @@
+package org.uma.jmetal.problem.singleobjective.trading;
+
+import org.ta4j.core.*;
+import org.ta4j.core.analysis.criteria.TotalProfitCriterion;
+import org.ta4j.core.analysis.criteria.VersusBuyAndHoldCriterion;
+import org.uma.jmetal.problem.impl.AbstractBinaryProblem;
+import org.uma.jmetal.problem.impl.AbstractIntegerProblem;
+import org.uma.jmetal.solution.BinarySolution;
+import org.uma.jmetal.solution.IntegerSolution;
+import org.uma.jmetal.solution.impl.DefaultBinarySolution;
+import ta4jexamples.research.MultipleStrategy;
+import ta4jexamples.strategies.BagovinoStrategy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.BitSet;
+import java.util.List;
+
+/**
+ * Class representing problem StockMarket. The problem consist of maximizing the
+ * profit in a tick serie
+ */
+@SuppressWarnings("serial")
+public class StockMarketInteger extends AbstractIntegerProblem {
+  // Getting a time series (from any provider: CSV, web service, etc.)
+  private TimeSeries series;
+  private static final int NUMBER_OF_PARAMETERS = 7;
+
+  /** Constructor */
+  /*
+  public StockMarket() {
+    this(256);
+  }
+  */
+
+  /** Constructor */
+  public StockMarketInteger() {
+    // Number of variables is the number of strategies at hand
+    setNumberOfVariables(NUMBER_OF_PARAMETERS);
+    setNumberOfObjectives(1);
+    List<Integer> lowerLimits = new ArrayList<>();
+    List<Integer> upperLimits = new ArrayList<>();
+    for(int i = 0; i < NUMBER_OF_PARAMETERS; i++) {
+      lowerLimits.add(1);
+      upperLimits.add(200);
+    }
+    setLowerLimit(lowerLimits);
+    setUpperLimit(upperLimits);
+    setName("StockMarketInteger");
+
+    //series = CsvTradesLoader.loadBitstampSeries();
+    series = CsvTicksLoader.load("EURUSD_Daily_201801020000_201812310000.csv");
+  }
+
+
+  /** Evaluate() method */
+  @Override
+  public void evaluate(IntegerSolution solution) {
+
+    // Running our juicy trading strategy...
+    TimeSeriesManager seriesManager = new TimeSeriesManager(series);
+
+    List<Strategy> strategies = new ArrayList<>();
+
+    strategies.add(Decoder.decode(series, solution.getVariables()));
+
+    MultipleStrategy multipleStrategy = new MultipleStrategy(strategies);
+
+    double profit = 0;
+
+    if(!multipleStrategy.getStrategies().isEmpty()) {
+        TradingRecord tradingRecord = seriesManager.run(multipleStrategy.buildStrategy(series));
+
+        // Getting the profitable trades ratio
+        //AnalysisCriterion profitTradesRatio = new AverageProfitableTradesCriterion();
+        //profit = profitTradesRatio.calculate(series, tradingRecord);
+
+        // Total profit of our strategy
+        // vs total profit of a buy-and-hold strategy
+        AnalysisCriterion vsBuyAndHold = new VersusBuyAndHoldCriterion(new TotalProfitCriterion());
+        profit = vsBuyAndHold.calculate(series, tradingRecord);
+    }
+
+    System.out.println("Profitable trades ratio: " + profit);
+
+    // StockMarket is a maximization problem: multiply by -1 to minimize
+    solution.setObjective(0, -1.0 * profit);
+
+    // OneMax is a maximization problem: multiply by -1 to minimize
+    //solution.setObjective(0, -1.0 * counterOnes);
+  }
+}
+
+
